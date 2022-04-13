bc9d92063d8f1f49372d8240883a03bc325ebfe9
sebasperey <spereyrairaola@gmail.com>
2022-04-13 11:54:51 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express =  require('express');
+const path = require('path');
+
+const app = express ();
+
+app.use( express.static(path.resolve(__dirname, './public')) );
+
+app.listen(3030, () => console.log('"Servidor corriendo en http://localhost:3030"'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/index.html'));
+})
